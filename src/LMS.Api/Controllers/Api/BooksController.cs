f4f11075a1aecdd9c3deccfc3fadf5dac029a2f6
Thanzeel Jalaldeen <thanzeel@inexisconsulting.com>
2023-03-28 16:50:56 +0530

    Add project files.

---
+++ src/LMS.Api/Controllers/Api/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Api.Controllers.Api;
+
+[Route("api/books")]
+[ApiController]
+public sealed class BooksController : ControllerBase
+{
+    public BooksController()
+    {
+    }
+
+    [HttpGet]
+    public ActionResult<IReadOnlyList<object>> GetBooks(IEnumerable<object> list)
+    {
+        var book1 = new { BookName = "Mowgli", PublishedOn = DateTime.Now };
+        var book2 = new { BookName = "ASB", PublishedOn = DateTime.Now };
+        var book3 = new { BookName = "ZXY", PublishedOn = DateTime.Now };
+
+
+        return Ok(new[] { book1, book2, book3 });
+    }
+}
